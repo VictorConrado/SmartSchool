e9dc740bd08b51627971a0bcb690f9e72e38e625
VictorConrado <estudantecomv@gmail.com>
2025-11-13 23:16:34 -0300

    feat: Criando as primeiras entidades do sistema

--- SmartSchoolAPI/Models/Professor.cs
+++ SmartSchoolAPI/Models/Professor.cs
@@ -2,5 +2,16 @@
 {
     public class Professor
     {
+        public Professor() { }
+        public Professor(int id, string nome)
+        {
+            Id = id;
+            Nome = nome;
+
+        }
+        public int Id { get; set; }
+        public string Nome { get; set; }
+
+        public IEnumerable<Disciplina> Disciplinas { get; set; }
     }
 }
